26adbd821e432479d14ee5520d6d2c86f098dae8
MRPrayh <80837351+MRPrayh@users.noreply.github.com>
2023-05-02 00:18:13 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,5 +1,5 @@
 from urlextract import URLExtract
-from streamlit_wordcloud import wordcloud
+from streamlit-wordcloud import wordcloud
 import pandas as pd
 from collections import Counter
 import emoji
