c27b51e1ffb8f8cf07f843571988ffe848de9955
MRPrayh <80837351+MRPrayh@users.noreply.github.com>
2023-05-02 01:03:40 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -46,18 +46,18 @@ def create_wordcloud(selected_user,df):
     temp = df[df['user'] != 'group_notification']
     temp = temp[temp['message'] != '<Media omitted>\n']
 
-    def remove_stop_words(message):
-        y = []
-        for word in message.lower().split():
-            if word not in stop_words:
-                y.append(word)
-        return " ".join(y)
-
+    #def remove_stop_words(message):
+    y = []
+    for word in message.lower().split():
+        if word not in stop_words:
+            y.append(word)
+            return " ".join(y)
+    """
     wc = wordcloud.visualize(width=500,height=500,min_font_size=10,background_color='white')
     temp['message'] = temp['message'].apply(remove_stop_words)
     df_wc = wc.generate(temp['message'].str.cat(sep=" "))
     return df_wc
-
+    """
 def most_common_words(selected_user,df):
 
     f = open('stop_hinglish.txt','r')
