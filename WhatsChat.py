5b97dec820c57b275c2dde62421791368bb9d509
MRPrayh <80837351+MRPrayh@users.noreply.github.com>
2024-04-07 09:02:31 +0530

    typecheck on file_uploader

--- WhatsChat.py
+++ WhatsChat.py
@@ -25,7 +25,7 @@ st.markdown("""
 # Sidebar
 st.sidebar.title("Get Your Chat Analytics")
 
-uploaded_file = st.sidebar.file_uploader("Choose a .txt Chat File")
+uploaded_file = st.sidebar.file_uploader("Choose a .txt Chat File", type='.txt')
 
 if uploaded_file is None:
     # Home Page
@@ -36,105 +36,101 @@ if uploaded_file is None:
     
 else:
     bytes_data = uploaded_file.getvalue()
-    try:
-        data = bytes_data.decode("utf-8")
-        df = preprocessor.preprocess(data)
-    
-        # fetch unique users
-        user_list = df['user'].unique().tolist()
-        user_list.remove('group_notification')
-        user_list.sort()
-        user_list.insert(0,"Overall")
+    data = bytes_data.decode("utf-8")
+    df = preprocessor.preprocess(data)
 
-        selected_user = st.sidebar.selectbox("Show analysis w.r.t.",user_list)
-        
-        if st.sidebar.button("Show Analysis"):
+    # fetch unique users
+    user_list = df['user'].unique().tolist()
+    user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0,"Overall")
 
-            # Stats Area
-            num_messages, words, num_media_messages, num_links = helper.fetch_stats(selected_user,df)
-            st.title("Top Statistics")
-            col1, col2, col3, col4 = st.columns(4)
+    selected_user = st.sidebar.selectbox("Show analysis w.r.t.",user_list)
 
-            with col1:
-                st.header("Total Messages")
-                st.title(num_messages)
-            with col2:
-                st.header("Total Words")
-                st.title(words)
-            with col3:
-                st.header("Media Shared")
-                st.title(num_media_messages)
-            with col4:
-                st.header("Links Shared")
-                st.title(num_links)
+    if st.sidebar.button("Show Analysis"):
 
-            # monthly timeline
-            st.title("Monthly Timeline")
-            timeline = helper.monthly_timeline(selected_user,df)
+        # Stats Area
+        num_messages, words, num_media_messages, num_links = helper.fetch_stats(selected_user,df)
+        st.title("Top Statistics")
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            st.header("Total Messages")
+            st.title(num_messages)
+        with col2:
+            st.header("Total Words")
+            st.title(words)
+        with col3:
+            st.header("Media Shared")
+            st.title(num_media_messages)
+        with col4:
+            st.header("Links Shared")
+            st.title(num_links)
+
+        # monthly timeline
+        st.title("Monthly Timeline")
+        timeline = helper.monthly_timeline(selected_user,df)
+        fig,ax = plt.subplots()
+        ax.plot(timeline['time'], timeline['message'],color='green')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        # daily timeline
+        st.title("Daily Timeline")
+        daily_timeline = helper.daily_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.plot(daily_timeline['only_date'], daily_timeline['message'], color='black')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        # activity map
+        st.title('Activity Map')
+        col1,col2 = st.columns(2)
+
+        with col1:
+            st.header("Most Busy Day")
+            busy_day = helper.week_activity_map(selected_user,df)
             fig,ax = plt.subplots()
-            ax.plot(timeline['time'], timeline['message'],color='green')
+            ax.bar(busy_day.index,busy_day.values,color='purple')
             plt.xticks(rotation='vertical')
             st.pyplot(fig)
 
-            # daily timeline
-            st.title("Daily Timeline")
-            daily_timeline = helper.daily_timeline(selected_user, df)
+        with col2:
+            st.header("Most Busy Month")
+            busy_month = helper.month_activity_map(selected_user, df)
             fig, ax = plt.subplots()
-            ax.plot(daily_timeline['only_date'], daily_timeline['message'], color='black')
+            ax.bar(busy_month.index, busy_month.values,color='orange')
             plt.xticks(rotation='vertical')
             st.pyplot(fig)
 
-            # activity map
-            st.title('Activity Map')
-            col1,col2 = st.columns(2)
+        st.title("Weekly Activity Map")
+        user_heatmap = helper.activity_heatmap(selected_user,df)
+        fig,ax = plt.subplots()
+        ax = sns.heatmap(user_heatmap)
+        st.pyplot(fig)
+
+        # finding the busiest users in the group(Group level)
+        if selected_user == 'Overall':
+            st.title('Most Busy Users')
+            x,new_df = helper.most_busy_users(df)
+            fig, ax = plt.subplots()
+
+            col1, col2 = st.columns(2)
 
             with col1:
-                st.header("Most Busy Day")
-                busy_day = helper.week_activity_map(selected_user,df)
-                fig,ax = plt.subplots()
-                ax.bar(busy_day.index,busy_day.values,color='purple')
+                ax.bar(x.index, x.values,color='red')
                 plt.xticks(rotation='vertical')
                 st.pyplot(fig)
-
             with col2:
-                st.header("Most Busy Month")
-                busy_month = helper.month_activity_map(selected_user, df)
-                fig, ax = plt.subplots()
-                ax.bar(busy_month.index, busy_month.values,color='orange')
-                plt.xticks(rotation='vertical')
-                st.pyplot(fig)
+                st.dataframe(new_df)
+                
+        # most common words
+        most_common_df = helper.most_common_words(selected_user,df)
 
-            st.title("Weekly Activity Map")
-            user_heatmap = helper.activity_heatmap(selected_user,df)
-            fig,ax = plt.subplots()
-            ax = sns.heatmap(user_heatmap)
-            st.pyplot(fig)
+        fig,ax = plt.subplots()
 
-            # finding the busiest users in the group(Group level)
-            if selected_user == 'Overall':
-                st.title('Most Busy Users')
-                x,new_df = helper.most_busy_users(df)
-                fig, ax = plt.subplots()
+        ax.barh(most_common_df[0],most_common_df[1])
+        plt.xticks(rotation='vertical')
 
-                col1, col2 = st.columns(2)
-
-                with col1:
-                    ax.bar(x.index, x.values,color='red')
-                    plt.xticks(rotation='vertical')
-                    st.pyplot(fig)
-                with col2:
-                    st.dataframe(new_df)
-                    
-            # most common words
-            most_common_df = helper.most_common_words(selected_user,df)
-
-            fig,ax = plt.subplots()
-
-            ax.barh(most_common_df[0],most_common_df[1])
-            plt.xticks(rotation='vertical')
-
-            st.title('Most Common Words')
-            st.pyplot(fig)
-            
-    except:
-        uploaded_file = None
+        st.title('Most Common Words')
+        st.pyplot(fig)
